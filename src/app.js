c598f2289400ae1da4eac5389c0480aa644f03cd
lukerhys <hello@lukemelvin.com>
2020-02-26 08:56:40 +0000

    Got very stuck

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#app',
+    data: {
+      currencies: [],
+      // rates: []
+    },
+    computed: {
+    },
+    mounted() {
+      this.fetchMoney();
+    },
+    methods: {
+      fetchMoney: function () {
+        const request = fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(payload => this.currencies = payload)
+        // .then(payload => this.rates = payload.rates)
+        // console.log(rates);
+      }
+    }
+  });
+});
